d6131cd22619d91fba05a4a232aeda13379211a1
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2023-01-28 21:25:47 +0700

    add promotion

--- lib/src/screen/promotion/promotion_page.dart
+++ lib/src/screen/promotion/promotion_page.dart
@@ -1,5 +1,10 @@
+import 'package:cauvongstore_mobile/src/model/product_model.dart';
 import 'package:cauvongstore_mobile/src/resources/app_color.dart';
+import 'package:cauvongstore_mobile/src/resources/const.dart';
+import 'package:cauvongstore_mobile/src/screen/home/item_card.dart';
 import 'package:flutter/material.dart';
+
+import '../detail/detail_page.dart';
 
 class PromotionPage extends StatelessWidget {
   @override
@@ -9,9 +14,153 @@ class PromotionPage extends StatelessWidget {
         title: Text('Promotion'),
         backgroundColor: AppColor.kPrimaryColor,
       ),
-      body: const Center(
-        child: Text('Promotion Page'),
-      ),
+      body: _PromotionPage(),
     );
+  }
+}
+
+class _PromotionPage extends StatefulWidget {
+  const _PromotionPage({super.key});
+
+  @override
+  State<_PromotionPage> createState() => __PromotionPageState();
+}
+
+class __PromotionPageState extends State<_PromotionPage> {
+  List<ProductModel> _products = <ProductModel>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _createProductData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: _products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                // crossAxisSpacing: 20,
+                mainAxisSpacing: 10,
+                mainAxisExtent: 120,
+              ),
+              itemBuilder: (context, index) => ItemCard(
+                product: _products[index],
+                press: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailPage(
+                      product: _products[index],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        )
+      ],
+    );
+  }
+
+  //Set cung data
+  void _createProductData() {
+    var list = <ProductModel>[
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+      ProductModel(
+          name: 'Lốc 4 hộp sữa dinh dưỡng',
+          id: 'Sữa các loại',
+          image:
+              'https://cdn.tgdd.vn/Products/Images/2386/80493/bhx/loc-4-hop-sua-tuoi-tiet-trung-khong-duong-dutch-lady-180ml-202104150826346937.jpg',
+          price: 10000,
+          description: 'Description of product',
+          categoryId: '1'),
+    ];
+    setState(() {
+      _products = list;
+    });
   }
 }
