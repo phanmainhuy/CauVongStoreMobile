e61daaf63b89fe1464fbcaea5d51019868510a8e
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2023-02-06 02:45:43 +0700

    change api

--- lib/src/resources/api.dart
+++ lib/src/resources/api.dart
@@ -1,20 +1,24 @@
+import 'package:cauvongstore_mobile/src/resources/const.dart';
+
 class API {
   // User
-  static const String postRegister = '/register';
-  static const String postLogin = '/login';
-  static const String postChangePassword = '/change-password';
+  static const String postRegister = '${Const.apiAddress}/register';
+  static const String postLogin = '${Const.apiAddress}/login';
+  static const String postChangePassword =
+      '${Const.apiAddress}/change-password';
 
   // Products
-  static const String getAllProducts = '/products';
-  static const String getProductDetailByID = '/products/';
-  static const String getAllPromotionProducts = '/products/promotion';
+  static const String getAllProducts = '${Const.apiAddress}/products';
+  static const String getProductDetailByID = '${Const.apiAddress}/products/';
+  static const String getAllPromotionProducts =
+      '${Const.apiAddress}/products/promotion';
 
   // Categories
-  static const String getAllCategories = '/categories/all';
-  static const String getSearchCategories = '/categories';
+  static const String getAllCategories = '${Const.apiAddress}/categories/all';
+  static const String getSearchCategories = '${Const.apiAddress}/categories';
 
   // Order
-  static const String postCreateOrder = '/order';
-  static const String getOrderByID = '/order/';
-  static const String getOrderListByUser = '/orders-by-user';
+  static const String postCreateOrder = '${Const.apiAddress}/order';
+  static const String getOrderByID = '${Const.apiAddress}/order/';
+  static const String getOrderListByUser = '${Const.apiAddress}/orders-by-user';
 }
